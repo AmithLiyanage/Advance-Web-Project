84c6296d83455bb850e7a161c8a579fec0cad4b7
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-11 14:02:51 +0530

    start

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -12,3 +12,10 @@ app.use(cors({origin: 'http://localhost:4200'}));//change port 3000 to 4200
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
 app.use('/users', userController);
+
+// new
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
