d92f98b71fbd7040efa10da1aeb280adbeebd09e
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-12 17:17:47 +0530

    add new form

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const { mongoose } = require('./db.js');
 var userController = require('./controllers/UserController.js');
+
 //new
-// const path = require('path');
-// const crypto = require('crypto');
-// const multer = require('multer');
-// const GridFsStorage  = require('multer-gridfs-storage');
-// const Grid = require('gridfs-stream');
-// const methodOverride = require('method-override');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
 
-//new end
 
+//init app
 var app = express();
+
+//--->new
+
+//EJS
+app.set('view engine', 'ejs');
+
+//public folder
+app.use(express.static('./public'));// public is folder name 
+
+app.get('/', (req, res) => resizeBy.render('index'));
+//end new<-----
+
+
+const port = 3000;
+
 app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:4200'}));//change port 3000 to 4200
 
-app.listen(3000, () => console.log('Server started at port : 3000'));
+app.listen(port, () => console.log(`Server started at port : ${port}`));
 
 app.use('/users', userController);
-
-// new
-
-//init gfs
-// let gfs;
-
-// app.set('view engine', 'ejs');
-
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
