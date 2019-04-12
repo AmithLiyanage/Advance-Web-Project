3b2422416a2662b5e3d16342f8a0b22f4a119c02
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-12 19:38:21 +0530

    make as images only

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -15,15 +15,37 @@ const path = require('path');
 //set Storage Engine
 const storage = multer.diskStorage({
   destination: './public/uploads/',
-  filename: function(req, file, cb){  //cd - callback
+  filename: function(req, file, cb){  //cb - callback
     cb(null, file.fieldname + '-' +Date.now() + path.extname(file.originalname));
   }
 });
 
 //init upload
 const upload = multer({
-  storage: storage
+  storage: storage,
+  limits:{fileSize: 1000000},//image size, 1000000 bytes(1Mb)
+  fileFilter: function(req, file, cb) { // for donot allow upload files
+    checkFileType(file, cb);
+  }
 }).single('myImage');// single - for upla]oad one image
+
+// check file type ---> function
+function checkFileType(file, cb){
+  // Allow extention(=ext)
+  const filetypes = /jpeg|jpg|png|gif/;
+
+  // check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  // check mime
+  const mimetype = filetypes.test(file.mimetype);// ex --->image/jpeg
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
 
 //EJS
 app.set('view engine', 'ejs');
@@ -35,7 +57,7 @@ app.post('/upload', (req, res) => {
   //res.send('test'); //for check
   upload(req, res, (err) => {
     if(err){
-      escape.render('index', {
+      res.render('index', {
         msg: err
       });
     } else {
