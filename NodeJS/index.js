3ead14b159478ee47e5adeb335259447ba261b0e
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-04-12 12:54:35 +0530

    add multer

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 
 const { mongoose } = require('./db.js');
 var userController = require('./controllers/UserController.js');
+//new
+// const path = require('path');
+// const crypto = require('crypto');
+// const multer = require('multer');
+// const GridFsStorage  = require('multer-gridfs-storage');
+// const Grid = require('gridfs-stream');
+// const methodOverride = require('method-override');
+
+//new end
 
 var app = express();
 app.use(bodyParser.json());
@@ -14,8 +23,12 @@ app.listen(3000, () => console.log('Server started at port : 3000'));
 app.use('/users', userController);
 
 // new
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+//init gfs
+// let gfs;
+
+// app.set('view engine', 'ejs');
+
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
