ea9a8e02d546aa13f31da714ca68d931a588b8d0
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-06-03 21:47:43 +0530

    maps ok

--- AngularApp/src/app/maps1/maps1.component.ts
+++ AngularApp/src/app/maps1/maps1.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Maps1Component implements OnInit {
 
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  click(event) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    console.log(event);
+  }
+
   constructor() { }
 
   ngOnInit() {
